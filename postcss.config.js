eb1cc5d9734a1a0449ce85e819c5f02c3a46a248
Luke <mackenzieluke94@gmail.com>
2020-04-25 17:30:42 -0400

    hopefully fixed hover issue

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   //   whitelist: ['dark-mode'],
 
   // Include any special characters you're using in this regular expression
-  //   defaultExtractor: (content) => content.match(/[\w-/:%.]+(?<!:)/g) || [],
+  defaultExtractor: (content) => content.match(/[\w-/:%.]+(?<!:)/g) || [],
 });
 
 module.exports = {
